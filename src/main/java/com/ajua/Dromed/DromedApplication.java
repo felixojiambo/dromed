c9fc55f5e18e8f44d7677dc293501c1744c50cbb
Felix Ojiambo <felixojiamboe@gmail.com>
2024-06-26 07:14:08 -0700

    Initial commit

---
+++ src/main/java/com/ajua/Dromed/DromedApplication.java
@@ -0,0 +1,14 @@
+package com.ajua.Dromed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DromedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DromedApplication.class, args);
+		System.out.println("Building a Drone Dispatch System for Medication Transport");
+	}
+
+}
---
+++ src/test/java/com/ajua/Dromed/DromedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajua.Dromed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DromedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
