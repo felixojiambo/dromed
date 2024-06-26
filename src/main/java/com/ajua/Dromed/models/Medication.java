e3ffa8bbbe32f5ec6ccb0a3fcae9f8b3104b1b0b
Felix Ojiambo <felixojiamboe@gmail.com>
2024-06-26 07:32:24 -0700

    medication entity

---
+++ src/main/java/com/ajua/Dromed/models/Medication.java
@@ -0,0 +1,27 @@
+package com.ajua.Dromed.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Medication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int weight;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    private String imageUrl;
+}
